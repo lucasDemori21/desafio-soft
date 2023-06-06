a34ee83bc70960f3c4340c81272ef3c17e61d542
Lucas Demori <lucas-demori2001@hotmail.com>
2023-06-05 21:46:38 -0300

    Commit Final

--- header.php
+++ header.php
@@ -57,7 +57,9 @@
                 <div class="modal-dialog">
                     <div class="modal-content" style="height: 500px; background: rgba(0,0,0,0.8); border: 1px solid #FFF;">
                         <div class="modal-header">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">
+                                <img style="width: 20px; height: 20px;" src="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23FFF'><path d='M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z'/></svg>">
+                            </button>
                         </div>
                         <div class="modal-body">
                             <iframe style="width: 100%; height: 100%;" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen src="https://www.youtube.com/embed/NLcTcsdO86U?rel=0" frameborder="0"></iframe>
@@ -69,7 +71,9 @@
                 <div class="modal-dialog">
                     <div class="modal-content" style="color:#FFF; background: rgba(0,0,0,0.8); border: 1px solid #FFF;">
                         <div class="modal-header">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">
+                                <img style="width: 20px; height: 20px;" src="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23FFF'><path d='M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z'/></svg>">
+                            </button>
                         </div>
                         <div class="modal-body">
                             <form>
@@ -91,7 +95,9 @@
                 <div class="modal-dialog">
                     <div class="modal-content" style="color:#FFF; background: rgba(0,0,0,0.8); border: 1px solid #FFF;">
                         <div class="modal-header">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" class="btn" data-bs-dismiss="modal" aria-label="Close">
+                                <img style="width: 20px; height: 20px;" src="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23FFF'><path d='M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z'/></svg>">
+                            </button>
                         </div>
                         <div class="modal-body">
                             <form>
